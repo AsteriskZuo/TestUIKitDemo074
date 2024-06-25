6d1c8720733a3415445aeed81565ef4262d72022
asteriskzuo <zuoyuhsywn@hotmail.com>
2024-06-25 18:00:46 +0800

    update typescript

--- App.tsx
+++ App.tsx
@@ -5,114 +5,109 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import * as React from 'react';
+import {Pressable, SafeAreaView, Text, View} from 'react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  Container,
+  ConversationDetail,
+  TextInput,
+  useChatContext,
+} from 'react-native-chat-uikit';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const appKey = 'easemob#easeim';
+const userId = 'du004';
+const userPs = '1';
+const peerId = 'du005';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+function SendMessage() {
+  const [page, setPage] = React.useState(0);
+  const [appkey, setAppkey] = React.useState(appKey);
+  const [id, setId] = React.useState(userId);
+  const [ps, setPs] = React.useState(userPs);
+  const [peer, setPeer] = React.useState(peerId);
+  const im = useChatContext();
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+  if (page === 0) {
+    return (
+      // 登录页面
+      <SafeAreaView style={{flex: 1}}>
+        <TextInput
+          placeholder="Please App Key."
+          value={appkey}
+          onChangeText={setAppkey}
+        />
+        <TextInput
+          placeholder="Please Login ID."
+          value={id}
+          onChangeText={setId}
+        />
+        <TextInput
+          placeholder="Please Login token or password."
+          value={ps}
+          onChangeText={setPs}
+        />
+        <TextInput
+          placeholder="Please peer ID."
+          value={peer}
+          onChangeText={setPeer}
+        />
+        <Pressable
+          onPress={() => {
+            console.log('test:zuoyu:login', id, ps);
+            im.login({
+              userId: id,
+              userToken: ps,
+              usePassword: true,
+              result: res => {
+                console.log('login result', res);
+                console.log('test:zuoyu:error', res);
+                if (res.isOk === true) {
+                  setPage(1);
+                }
+              },
+            });
+          }}>
+          <Text>{'Login'}</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            im.logout({
+              result: () => {},
+            });
+          }}>
+          <Text>{'Logout'}</Text>
+        </Pressable>
+      </SafeAreaView>
+    );
+  } else if (page === 1) {
+    // 聊天页面
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <ConversationDetail
+          convId={peer}
+          convType={0}
+          onBack={() => {
+            setPage(0);
+            im.logout({
+              result: () => {},
+            });
+          }}
+          type={'chat'}
+        />
+      </SafeAreaView>
+    );
+  } else {
+    return <View />;
+  }
 }
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+  // 初始化 UIKit
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Container options={{appKey: appKey, autoLogin: false}}>
+      <SendMessage />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
